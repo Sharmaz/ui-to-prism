efbf37290ab77278983c7cd69650d1843e9915fe
Ivan Robles <irae45@gmail.com>
2023-11-30 20:13:26 -0600

    remove highligth.js.

--- src/components/snippet.tsx
+++ src/components/snippet.tsx
@@ -1,11 +1,10 @@
-import Highlight from 'react-highlight';
 import { TableElement } from '../types/definitions';
-import 'highlight.js/styles/atom-one-dark.min.css';
 
 const Snippet = ({ tables }: TableElement[] | any) => (
-  <div className="block w-2/3 h-full mt-4">
-    <Highlight className="">
-      {`
+  <div className="block text-slate-200 bg-slate-600 w-2/3 h-full mt-4">
+    <pre>
+      <code>
+        {`
           datasource db {
             provider = "postgresql"
             url      = env("DATABASE_URL")
@@ -14,33 +13,34 @@ const Snippet = ({ tables }: TableElement[] | any) => (
           generator client {
             provider = "prisma-client-js"
           }`}
-      {
-      tables.map((table) => (
-        <div key={table.id} className="ml-24 mt-6">
-          <span>model</span>
-          <span>{` ${table.name} {`}</span>
-          {
-            table.properties.map((attr) => (
-              <div key={attr.id} className="flex ml-8">
-                <span className="w-1/6">{attr.name}</span>
-                <span>{attr.type}</span>
-              </div>
-            ))
-          }
-          {
-            table.relationships.map((rel) => (
-              <div key={table.relationships.length + rel.id} className="flex ml-8">
-                <span className="w-1/6">{rel.name}</span>
-                <span className="w-1/6">{rel.table}</span>
-                <span>{`@relation(fields: [${rel.field}], refereces:[${rel.reference}])`}</span>
-              </div>
-            ))
-          }
-          <span>{'}'}</span>
-        </div>
-      ))
-      }
-    </Highlight>
+        {
+        tables.map((table) => (
+          <div key={table.id} className="ml-24 mt-6">
+            <span>model</span>
+            <span>{` ${table.name} {`}</span>
+            {
+              table.properties.map((attr) => (
+                <div key={attr.id} className="flex ml-8">
+                  <span className="w-1/6">{attr.name}</span>
+                  <span>{attr.type}</span>
+                </div>
+              ))
+            }
+            {
+              table.relationships.map((rel) => (
+                <div key={table.relationships.length + rel.id} className="flex ml-8">
+                  <span className="w-1/6">{rel.name}</span>
+                  <span className="w-1/6">{rel.table}</span>
+                  <span>{`@relation(fields: [${rel.field}], refereces:[${rel.reference}])`}</span>
+                </div>
+              ))
+            }
+            <span>{'}'}</span>
+          </div>
+        ))
+        }
+      </code>
+    </pre>
   </div>
 );
 
